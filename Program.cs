1c9568b01c1a2ad9e824120874803cd7acf34bca
Bar Horing <bahoring@microsoft.com>
2018-06-10 16:53:19 +0300

    c# basics - initial commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Treehouse.FitnessFrog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("Enter how many minutes you exercised: ");
+            // string entry = Console.ReadLine();
+            // Console.WriteLine(entry);
+            // Console.WriteLine("You've exercised " + entry + " minutes");
+            string firstName = Console.ReadLine() +  " rocks!";
+            Console.WriteLine(firstName);
+        }
+    }
+}
