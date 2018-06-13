34e21d4e874c6b8b93fcad9fccb76a7778edbc14
Bar Horing <bahoring@microsoft.com>
2018-06-13 17:28:49 +0300

    in progess... to coninue from home

--- Program.cs
+++ Program.cs
@@ -6,12 +6,17 @@ namespace Treehouse.FitnessFrog
     {
         static void Main(string[] args)
         {
-            // Console.WriteLine("Enter how many minutes you exercised: ");
-            // string entry = Console.ReadLine();
-            // Console.WriteLine(entry);
-            // Console.WriteLine("You've exercised " + entry + " minutes");
-            string firstName = Console.ReadLine() +  " rocks!";
-            Console.WriteLine(firstName);
+            int runningTotal = 0;
+            bool keepGoing = true;
+            while(keepGoing){
+                Console.WriteLine("Enter how many minutes you exercised or type 'quit' to exit:");
+                string entry = Console.ReadLine();
+                int minutes = int.Parse(entry);
+                runningTotal += minutes;
+                Console.WriteLine(entry);
+                Console.WriteLine("You've exercised " + runningTotal + " minutes");
+            }
+
         }
     }
 }
