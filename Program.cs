adfdf5577bd6c6164f7dbbe429af2590bf15f7a5
Bar Horing <bar.apple@gmail.com>
2018-06-19 23:14:14 +0300

    from home

--- Program.cs
+++ Program.cs
@@ -6,41 +6,41 @@ namespace Treehouse.FitnessFrog
     {
         static void Main(string[] args)
         {
-            int runningTotal = 0;
+            double runningTotal = 0;
             bool keepGoing = true;
             while(keepGoing){
                 Console.WriteLine("Enter how many minutes you exercised or type \"quit\" to exit:");
                 string entry = Console.ReadLine();
-                if(entry == "quit"){
+                if(entry.ToLower() == "quit"){
                     keepGoing = false;
                 }
                 else {
                     try {
-                        int minutes = int.Parse(entry);
+                        double minutes = double.Parse(entry);
+                            
+                        if(minutes <= 0){
+                            Console.WriteLine(minutes + " is not an acceptable value.");
+                            continue;
+                        }
+                        else if(minutes <=10){
+                            Console.WriteLine("Better than nothing, am I right?");
+                        }
+                        else if(minutes <= 30){
+                            Console.WriteLine("Way to go hot stuff!");        
+                        }
+                        else if(minutes <=60){
+                            Console.WriteLine("You must be a ninja warrior in training!");
+                        }
+                        else if(minutes > 60){
+                            Console.WriteLine("Okay, now you're just showing off!");
+                        }
+                        runningTotal += minutes;
+                        Console.WriteLine("You've exercised " + runningTotal + " minutes");
                     }
-                    catch(e) {
+                    catch(FormatException) {
                         Console.WriteLine("That is not a valid input");
                         continue;
                     }
-
-                    if(minutes <= 0){
-                        Console.WriteLine(minutes + " is not an acceptable value.");
-                        continue;
-                    }
-                    else if(minutes <=10){
-                        Console.WriteLine("Better than nothing, am I right?");
-                    }
-                    else if(minutes <= 30){
-                        Console.WriteLine("Way to go hot stuff!");        
-                    }
-                    else if(minutes <=60){
-                        Console.WriteLine("You must be a ninja warrior in training!");
-                    }
-                    else if(minutes > 60){
-                        Console.WriteLine("Okay, now you're just showing off!");
-                    }
-                    runningTotal += minutes;
-                    Console.WriteLine("You've exercised " + runningTotal + " minutes");
                 }
 
             }
