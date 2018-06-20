2ed09ec842d57a1de47580db21f0a4ae62822e14
Bar Horing <bahoring@microsoft.com>
2018-06-20 15:19:11 +0300

    done

--- Program.cs
+++ Program.cs
@@ -7,42 +7,40 @@ namespace Treehouse.FitnessFrog
         static void Main(string[] args)
         {
             double runningTotal = 0;
-            bool keepGoing = true;
-            while(keepGoing){
+            double minutes;
+            string entry;
+            while(true){
                 Console.WriteLine("Enter how many minutes you exercised or type \"quit\" to exit:");
-                string entry = Console.ReadLine();
+                entry = Console.ReadLine();
                 if(entry.ToLower() == "quit"){
-                    keepGoing = false;
+                    break;
                 }
-                else {
-                    try {
-                        double minutes = double.Parse(entry);
-                            
-                        if(minutes <= 0){
-                            Console.WriteLine(minutes + " is not an acceptable value.");
-                            continue;
-                        }
-                        else if(minutes <=10){
-                            Console.WriteLine("Better than nothing, am I right?");
-                        }
-                        else if(minutes <= 30){
-                            Console.WriteLine("Way to go hot stuff!");        
-                        }
-                        else if(minutes <=60){
-                            Console.WriteLine("You must be a ninja warrior in training!");
-                        }
-                        else if(minutes > 60){
-                            Console.WriteLine("Okay, now you're just showing off!");
-                        }
-                        runningTotal += minutes;
-                        Console.WriteLine("You've exercised " + runningTotal + " minutes");
-                    }
-                    catch(FormatException) {
-                        Console.WriteLine("That is not a valid input");
+                try {
+                    minutes = double.Parse(entry);
+                        
+                    if(minutes <= 0){
+                        Console.WriteLine(minutes + " is not an acceptable value.");
                         continue;
                     }
+                    else if(minutes <=10){
+                        Console.WriteLine("Better than nothing, am I right?");
+                    }
+                    else if(minutes <= 30){
+                        Console.WriteLine("Way to go hot stuff!");        
+                    }
+                    else if(minutes <=60){
+                        Console.WriteLine("You must be a ninja warrior in training!");
+                    }
+                    else if(minutes > 60){
+                        Console.WriteLine("Okay, now you're just showing off!");
+                    }
+                    runningTotal += minutes;
+                    Console.WriteLine("You've exercised " + runningTotal + " minutes");
                 }
-
+                catch(FormatException) {
+                    Console.WriteLine("That is not a valid input");
+                    continue;
+                }
             }
             Console.WriteLine("goodbye");
 
